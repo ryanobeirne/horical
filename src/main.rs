f67eb55820f1c6bbe919c127598612ac49b59992
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2021-01-05 21:06:51 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use holiday::*;
+use std::fmt;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let mut std_out = std::io::stdout();
+    write!(&mut std_out, "{}", HoriCal::today())?;
+
+    Ok(())
+}
+
+struct HoriCal {
+    weekdays: Vec<Weekday>,
+    days: Vec<u32>,
+    today: u32,
+}
+
+impl fmt::Display for HoriCal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let weekdays = self.weekdays
+            .iter()
+            .enumerate()
+            .map(|(i, wkd)| {
+                let wkd = TwoLetterWkday::from(wkd);
+                if i == self.today as usize {
+                    format!("${{color0}}{}${{color}}", wkd)
+                } else if wkd.is_weekend() {
+                    format!("${{color1}}{}${{color}}", wkd)
+                } else {
+                    wkd.to_string()
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("  ");
+
+        let  days = self.days
+            .iter()
+            .enumerate()
+            .map(|(i, day)| {
+                if i == self.today  as usize{
+                    format!("${{color0}}{:02}${{color}}", day)
+                } else {
+                    format!("{:02}", day)
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("  ");
+
+        writeln!(f, "{}\n{}", weekdays, days)
+    }
+}
+
+impl HoriCal {
+    fn today() -> Self {
+        let mut weekdays = Vec::new();
+        let mut days = Vec::new();
+
+        let today = Local::today();
+        let mut i = today.with_day0(0).unwrap().naive_local();
+        let last = today.last_day_of_month();
+
+        while i <= last {
+            weekdays.push(i.weekday());
+            days.push(i.day());
+            i = i.succ();
+        }
+
+
+        HoriCal {
+            weekdays,
+            days,
+            today: today.day0(),
+        }
+    }
+}
+
+use TwoLetterWkday::*;
+#[derive(Debug, PartialEq, Eq)]
+enum TwoLetterWkday {
+    SU, MO, TU, WE, TH, FR, SA,
+}
+
+impl TwoLetterWkday {
+    fn is_weekend(&self) -> bool {
+        self == &SA || self == &SU
+    }
+}
+
+impl fmt::Display for TwoLetterWkday {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl From<&Weekday> for TwoLetterWkday {
+    fn from(weekday: &Weekday) -> Self {
+        use Weekday::*;
+        match weekday {
+            Sun => SU,
+            Mon => MO,
+            Tue => TU,
+            Wed => WE,
+            Thu => TH,
+            Fri => FR,
+            Sat => SA,
+        }
+    }
+}
